3acfec93118a45258479a2dcd81076ce35e9c526
driely01 <elyaagoubi.works@gmail.com>
2024-02-08 10:42:22 +0100

    remove duplicate files

--- ex03/ex02/find_ft_type.py
+++
@@ -1,8 +0,0 @@
-def all_things_is_obj(object: any) -> int:
-    whichtype = type(object)
-    splited = ((str(whichtype).split(" "))[1].split("'"))[1].capitalize()
-    if splited == "List" or splited == "Tuple" or splited == "Set" or splited == "Dict" or splited == "Str":
-        print(splited, ": ", whichtype)
-    else:
-        print("Type not found")
-    return 42
