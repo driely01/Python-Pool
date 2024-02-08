e0fa48f6f1ba4a33c0d396a29bdbfd8aebed9fdf
driely01 <elyaagoubi.works@gmail.com>
2024-02-08 10:40:13 +0100

    solve ex00, ex01 and ex02

---
+++ ex00/Hello.py
@@ -0,0 +1,10 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Morocco")
+ft_set = {"Hello", "Khouribga"}
+ft_dict = {"Hellp" : "1337"}
+
+# your code here
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
